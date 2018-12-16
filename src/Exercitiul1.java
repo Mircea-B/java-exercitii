5842c459623f60a87872d1cc94d537f93986c823
Mircea <bodea.mircea@gmail.com>
2018-12-16 02:12:24 +0200

    Asta este primul commit, pentru o serie de exercitii, ce le voi face in cadrul acestui Proiect, - exercitiile vor fi rezolvate utilizand obiecte si metode, Have a nice day

---
+++ src/Exercitiul1.java
@@ -0,0 +1,9 @@
+// va fi main-ul pentru niste exercitii pe care le voi rezolva utilizand obiecte si metode
+public class Exercitiul1 {
+    public static void main(String[] args) {
+
+// aici voi introduce codul :D
+
+    }
+
+}

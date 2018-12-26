28ad91d2bad3e7bfe9de892535832278c0444455
Mircea <bodea.mircea@gmail.com>
2018-12-26 17:04:11 +0200

    am  adougat niste comentarii in README

---
+++ src/Exercitiul2.java
@@ -0,0 +1,7 @@
+// 2.scrieti un alt algoritm de sortare, preferabil pe care il ganditi voi. nu e important sa fie optim.
+// citit despre insertion sort si il rescrieti pe hartie/cod
+
+public class Exercitiul2 {
+    // aici voi efectua exercitiul 2
+
+}
